c7f0c0bbd95eff3a4b600c0f71fe41203d7ee526
Julvensly Destine <destinejulvens16@gmail.com>
2026-08-09 11:45:59 -0400

    mise à jour

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,50 +1,42 @@
 import { useContext } from "react";
-import { AuthContext } from "../AuthContext";
-import { Link, useNavigate } from 
-"react-router-dom";
- 
- function Navbar() {
-  const { isLoggedIn, role, logout } =
-useContext(AuthContext);
- const navigate = useNavigate();
- const deconnexion = () => {
- logout();
-navigate("/connexion");
+ import { AuthContext } from "../AuthContext";
+import { Link, useNavigate } from "react-router-dom"; 
+function Navbar() {
+  const { isLoggedIn, role, logout } = useContext(AuthContext);
+ const navigate =  useNavigate(); const deconnexion = () => {
+    logout(); navigate("/connexion");
   };
   return (
  <nav>
  <h2>Jule Top Up</h2>
  <div>
-        <Link to="/">Accueil</Link>{" | "}
- {isLoggedIn ? (role === "admin" ? (
- <> 
-              <Link to="/compte-admin">👤 Admin</Link>{" | "} 
-              <button 
-              onClick={deconnexion}>
-                Déconnexion 
-              </button>
-            </>
+ <Link  to="/">Accueil</Link>{" | "}
+ {isLoggedIn ? ( role === "admin" ? ( <>
+ <Link  to="/compte-admin">👤 Admin</Link>{" | "} 
+              <button onClick={deconnexion}>
+                Déconnexion </button>
+ </>
+ ) : role === 
+          "semiadmin" ? (
+            <> <Link to="/compte-semi-admin"> 👤  Semiadmin
+              </Link>{" | "}
+ <button onClick={deconnexion}>
+                Déconnexion </button> </>
  ) : (
  <>
- <Link to="/compte">👤 Mon  compte</Link>{" | "}
- <button  onClick={deconnexion}>
-                Déconnexion 
-              </button>
-            </>
-  ) : ( 
-<>
-<Link to="/compte-semi-admin">👤 semiadmin</Link>{" | "}
-<button onClick={deconnexion}> Deconnexion</button>
-</>
-)
-) : (
-<> 
-<Link to="/inscrit">S'inscrire</Link>{" | "}
-            <Link to="/connexion"> connecter</Link>
+ <Link to="/compte">👤 Mon compte</Link>{" | "} 
+              <button onClick={deconnexion}>
+                Déconnexion </button>
+ </>
+ )
+ ) : (
+ <> 
+            <Link to="/inscrit">S'inscrire</Link>{" | "} 
+            <Link to="/connexion">Se connecter</Link>
           </>
- )} 
-</div>
- </nav> 
-);
+ )}
+ </div>
+ </nav>
+ );
 }
 export default Navbar;
