b4f3ec68181a452b612341cc627eca4f2509502a
Julvensly Destine <destinejulvens16@gmail.com>
2026-08-18 18:07:21 -0400

    Add community route

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const express = require("express");
  const mongoose = require("mongoose");
  const dotenv = require("dotenv"); dotenv.config();
  const orderRoutes = require("./routes/orderRoutes");
-  const app = express();
 const messageRoutes = require("./routes/messageRoutes");
 const userRoutes = require("./routes/userRoutes");
+const communityRoutes = require("./routes/communityRoutes");
+const app = express();
  app.use(cors());
  app.use(express.json());
 app.use("/api/orders", orderRoutes);
  app.use("/api/messages", messageRoutes); 
 app.use("/api/users", userRoutes);
- const PORT = process.env.PORT || 5000; 
+app.use("/api/community", communityRoutes); 
+const PORT = process.env.PORT || 5000; 
  mongoose
   .connect(process.env.MONGO_URI)
  .then(() => { console.log("MongoDB konekte  avèk siksè");
